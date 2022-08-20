ef1f7f476bd8e98b15c7642f03c3408452542933
Michael Runzler <ap792a@gmail.com>
2022-08-20 02:36:47 -0700

    + Continued working on base classes for BE-based machines

--- src/main/java/michaelrunzler/fluiddynamics/block/FDMachineBase.java
+++ src/main/java/michaelrunzler/fluiddynamics/block/FDMachineBase.java
@@ -48,35 +48,75 @@ public abstract class FDMachineBase extends Block implements EntityBlock
         this.type = type;
     }
 
+    /**
+     * Overriding classes should not modify this method. unless they need a specific render shape.
+     * @param state the blockstate that is being evaluated for its render shape
+     * @param getter used to get the block/BE from the BlockPos
+     * @param pos the position of the block/BE being modified
+     * @return the current state's occlusion shape
+     */
     @SuppressWarnings("deprecation")
     @Override
     public @NotNull VoxelShape getOcclusionShape(@NotNull BlockState state, @NotNull BlockGetter getter, @NotNull BlockPos pos) {
         return RENDER_SHAPE;
     }
 
+    /**
+     * Overriding classes should use this method to generate a BlockEntity for a given BlockState by instantiating a new
+     * instance of the associated BlockEntity subclass.
+     * @param pos the position of the block being queried
+     * @param state the current BlockState
+     */
     @Nullable
     @Override
     public abstract BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state);
 
+    /**
+     * Overriding classes should use this method to tick either the client or server depending on what they need to do.
+     * Typically, machines only need to tick the server for inventory and BE updates, so nothing should be done for
+     * level.isClientSide() == true. Otherwise, tick the server if the associated BE is the BE being ticked.
+     * @param level the level doing the tick
+     * @param state the current block state
+     * @param type a reference to the BE being ticked
+     * @return a lambda which ticks the server under specific conditions
+     */
     @Nullable
     @Override
     public abstract <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> type);
 
+    /**
+     * Gets the default BlockState when a block is first placed. This defaults to POWERED = false, but additional states
+     * can be added by overriding this method and modifying the State returned by super(ctx).
+     * @return the completed BlockState
+     */
     @SuppressWarnings("ConstantConditions")
     @Nullable
     @Override
     public BlockState getStateForPlacement(@NotNull BlockPlaceContext ctx) {
-        // This should be overridden by subclasses which need additional states
         return super.getStateForPlacement(ctx).setValue(BlockStateProperties.POWERED, false);
     }
 
+    /**
+     * Treated as above; call builder.add(StateProperty) after calling super(builder) to add more properties.
+     */
     @Override
-    protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder) {
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         // Override as above for more properties
         super.createBlockStateDefinition(builder);
         builder.add(BlockStateProperties.POWERED);
     }
 
+    /**
+     * Called when a Player right-clicks on the block. Overriding classes should use this method to open a GUI or change
+     * states as required, typically using a GUIProvider lambda.
+     * @param state the current state of the block
+     * @param level the level in which the right-click is being performed
+     * @param pos the position of the block being clicked
+     * @param player the player clicking the block
+     * @param hand the hand being used to use the block
+     * @param result the actual use interaction result
+     * @return the result of the interaction; typically InteractionResult.SUCCESS if nothing went wrong
+     */
     @Override
     public abstract InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos,
                                  @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult result);
--- src/main/java/michaelrunzler/fluiddynamics/blockentity/MachineBlockEntityBase.java
+++ src/main/java/michaelrunzler/fluiddynamics/blockentity/MachineBlockEntityBase.java
@@ -5,11 +5,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
-import org.checkerframework.checker.units.qual.A;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -24,12 +22,22 @@ public abstract class MachineBlockEntityBase extends BlockEntity
     protected MachineEnum type;
     protected ArrayList<LazyOptional<?>> optionals;
 
+    /**
+     * Overriding classes should declare LazyOptionals containing Handlers for each function that the BE provides
+     * (storage, power, RS output, etc) and load/unload data from them in the load() and saveAdditional() methods.
+     * All of these handlers must be declared as capabilities in the getCapability() method.
+     */
     public MachineBlockEntityBase(BlockPos pos, BlockState state, MachineEnum type) {
         super(ModBlockEntities.registeredBEs.get(type.name().toLowerCase()).get(), pos, state);
 
         this.optionals = new ArrayList<>();
+
+        this.type = type;
     }
 
+    /**
+     * Subclasses should not override this method unless they need to do something special when the BE is removed.
+     */
     @Override
     public void setRemoved() {
         super.setRemoved();
@@ -37,16 +45,28 @@ public abstract class MachineBlockEntityBase extends BlockEntity
         for(LazyOptional<?> o : optionals) o.invalidate();
     }
 
+    /**
+     * Overriding classes should use this method to load NBT data which was stored when the block was unloaded.
+     * @param tag the NBT data to load from
+     */
     @Override
-    public void load(CompoundTag p_155245_) {
-        super.load(p_155245_); // TODO stopped here
-    }
+    public abstract void load(@NotNull CompoundTag tag);
 
+    /**
+     * Overriding classes should use this method to save any data which needs to be stored while the block is unloaded.
+     * @param tag the NBT tag to save data to
+     */
     @Override
-    protected void saveAdditional(CompoundTag p_187471_) {
-        super.saveAdditional(p_187471_);
-    }
+    protected abstract void saveAdditional(@NotNull CompoundTag tag);
 
+    /**
+     * Subclasses should declare all additional capabilities here, with each being represented as a LazyOptional cast
+     * to its proper type on return. Directional handling is also possible (I-sidedness) via the provided Direction.
+     * @param cap The capability to check
+     * @param side The Side to check from, with null being internal checks (i.e. from the BE itself)
+     * @return the LazyOptional containing the handler for the given capability and side, or a call to super if none
+     * is available
+     */
     @NotNull
     @Override
     public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
