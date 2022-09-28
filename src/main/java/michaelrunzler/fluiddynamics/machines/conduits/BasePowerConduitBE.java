c08084d2a83079c57a7c0b19de49848d0f341e84
Ethan R. Scott <ap792a@gmail.com>
2022-09-27 19:13:00 -0700

    + Made changes to the routing logic for the basic power conduit - it now uses a round-robin distribution algorithm for forwarding, and a smart balance-routing algorithm for packet acceptance. Balancing for BE insertion and testing is pending.

--- src/main/java/michaelrunzler/fluiddynamics/machines/conduits/BasePowerConduitBE.java
+++ src/main/java/michaelrunzler/fluiddynamics/machines/conduits/BasePowerConduitBE.java
@@ -8,29 +8,35 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
+import java.util.HashMap;
 
 public abstract class BasePowerConduitBE extends PoweredMachineBE
 {
     protected static final String PACKET_NBT_TAG = "EnergyPacket";
     protected static final String PACKET_DIR_TAG = "PacketDir";
-    protected static final String PACKET_IMPORT_TAG = "PacketImported";
     protected static final String PACKET_AMT_TAG = "PacketAmount";
+    protected static final int MAX_CONSECUTIVE_INSERTIONS = 2;
     protected ConduitPowerPacket storedPacket;
-    protected Direction lastSideAccess;
-    protected Random rng;
+    protected Direction lastSideAccess; // The direction from which the conduit's power API was last accessed by another BE
+    protected Direction lastPacketInsertion; // The direction from which a packet was last accepted
+    protected int nextForwardDir; // The next direction ordinal that the conduit should attempt to forward held packets to
+    protected int sequentialInsertionCount; // The number of times the conduit has received a packet from the same side sequentially
 
     public BasePowerConduitBE(BlockPos pos, BlockState state, MachineEnum type) {
         super(pos, state, type, true, true, false);
-        rng = new Random();
         storedPacket = null;
         lastSideAccess = null;
+        lastPacketInsertion = null;
+        nextForwardDir = 0;
+        sequentialInsertionCount = 0;
     }
 
     @SuppressWarnings("unchecked")
@@ -50,13 +56,21 @@ public abstract class BasePowerConduitBE extends PoweredMachineBE
     {
         // Transform any power pushed into this conduit by external machines into a power packet
         if(storedPacket == null && energyHandler.getEnergyStored() > 0) {
-            storedPacket = new ConduitPowerPacket(lastSideAccess, energyHandler.getEnergyStored(), true);
+            storedPacket = new ConduitPowerPacket(lastSideAccess, energyHandler.getEnergyStored());
             energyHandler.extractEnergy(energyHandler.getEnergyStored(), false);
         }
 
         // Transfer the stored power packet (if any) to neighboring energy-capable blocks
-        // Shuffle the direction array first
-        exportToNeighbors(shuffle(Direction.values()));
+        boolean successful;
+        int looped = nextForwardDir;
+        Direction[] values = Direction.values();
+        do {
+            // Starting with the next direction, iterate through the list of directions until we hit the original nextDir
+            // or succeed with a power transfer (round-robin switching)
+            successful = exportToNeighbor(values[nextForwardDir]);
+            nextForwardDir++;
+            if(nextForwardDir >= values.length) nextForwardDir = 0;
+        }while(!successful && nextForwardDir != looped);
     }
 
     @Override
@@ -67,7 +81,7 @@ public abstract class BasePowerConduitBE extends PoweredMachineBE
         if(tag.contains(PACKET_NBT_TAG)) {
             CompoundTag packetData = tag.getCompound(PACKET_NBT_TAG);
             storedPacket = new ConduitPowerPacket(Direction.byName(packetData.getString(PACKET_DIR_TAG)),
-                    tag.getInt(PACKET_AMT_TAG), tag.getBoolean(PACKET_IMPORT_TAG));
+                    tag.getInt(PACKET_AMT_TAG));
         }
     }
 
@@ -80,13 +94,12 @@ public abstract class BasePowerConduitBE extends PoweredMachineBE
         {
             CompoundTag pTag = new CompoundTag();
             pTag.putString(PACKET_DIR_TAG, storedPacket.origin().getName());
-            pTag.putBoolean(PACKET_IMPORT_TAG, storedPacket.imported());
             pTag.putInt(PACKET_AMT_TAG, storedPacket.amount());
         }
     }
 
     /**
-     * This is overridden for a somewhat complicated reason, which I will attempt to explain here for future me to
+     * This is written as it is for a somewhat complicated reason, which I will attempt to explain here for future me to
      * (hopefully) understand.
      * Normally, blocks transmit "packets" of energy to their neighbors by directly inserting power into their neighbors'
      * energy storages and subtracting it from their own. This works great for block-to-block transfer, since the only
@@ -97,47 +110,69 @@ public abstract class BasePowerConduitBE extends PoweredMachineBE
      * until they reach a non-conduit device. Since these "packets" have extra directional data associated with them that
      * tells the conduit which direction they came from, they don't run into the same looping issue that actual energy
      * transfer has.
+     * ===
+     * To address some additional issues with this system (namely uneven distribution), we also add two further characteristics
+     * to the packet network.
+     * First, we use a "round-robin" output system to evenly distribute incoming packets to all outgoing
+     * connections. For 1-in, 1-out (straight-through) connections, this is a minor efficiency penalty.
+     * Second, we use a "routing/forwarding" scheme for incoming packets. This evaluates whether the node is a straight-through
+     * or multi-input node, and acts accordingly. For straight-through, the conduit just blindly accepts all packets that
+     * will fit into its cache and forwards them out as normal. For multi-input, the conduit "holds" inputs on an alternating
+     * cycle, ensuring that all feeders can get an opportunity to deliver their packets.
      */
-    @Override
-    protected void exportToNeighbors(Direction... dirs)
+    protected boolean exportToNeighbor(Direction d)
     {
-        if(level == null) return;
+        // Ensure that the given direction is valid and has a connection; that we have a packet to give out; and that
+        // said packet did not originate from the given direction.
+        if(level == null || storedPacket == null || d == null || storedPacket.origin() == d ||
+                !this.getBlockState().getValue(dirMap.get(d))) return false;
 
-        // TODO not evenly distributing power, check TE maybe?
-        for(Direction d : dirs)
+        // Grab the BE at the position and ensure it's a valid network component
+        BlockEntity rbe = this.level.getBlockEntity(this.worldPosition.relative(d));
+        if(rbe instanceof PoweredMachineBE pbe && pbe.canImportPower)
         {
-            // Don't bother checking more neighbors if there is no "energy" to distribute
-            if(storedPacket == null) return;
-
-            // Only attempt to "forward" energy to this side if the current packet wasn't generated from this side
-            if(storedPacket.origin() == d) continue;
-
-            // Grab the BE at the position and ensure it's a valid network component
-            BlockEntity rbe = this.level.getBlockEntity(this.worldPosition.relative(d));
-            if(rbe == null) continue;
-            if(rbe instanceof PoweredMachineBE pbe && pbe.canImportPower)
-            {
-                // If the BE is a conduit, handle its transfer with the packetization system
-                if(pbe instanceof BasePowerConduitBE cbe) {
-                    if(cbe.tryAcceptPacket(storedPacket, d.getOpposite())) storedPacket = null; // Clear the stored packet if transfer was successful
-                }else {
-                    // Otherwise, export the current packet to the BE. If the BE can't take all the packet's energy,
-                    // just export what it can take and update the packet
-                    int xfered = pbe.getCapability(CapabilityEnergy.ENERGY).map(c -> c.receiveEnergy(storedPacket.amount(), false)).orElse(0);
-                    if(xfered == storedPacket.amount()) storedPacket = null;
-                    else storedPacket = new ConduitPowerPacket(storedPacket.origin(), storedPacket.amount() - xfered, storedPacket.imported());
+            // If the BE is a conduit, handle its transfer with the packetization system
+            if(pbe instanceof BasePowerConduitBE cbe) {
+                if(cbe.tryAcceptPacket(storedPacket, d.getOpposite())) {
+                    storedPacket = null; // Clear the stored packet if transfer was successful
+                    return true;
                 }
+            }else
+            {
+                // Otherwise, export the current packet to the BE. If the BE can't take all the packet's energy,
+                // just export what it can take and update the packet
+                int xfered = pbe.getCapability(CapabilityEnergy.ENERGY).map(c -> c.receiveEnergy(storedPacket.amount(), false)).orElse(0);
+
+                if(xfered == storedPacket.amount()) storedPacket = null;
+                else if(xfered == 0) return false;
+                else storedPacket = new ConduitPowerPacket(storedPacket.origin(), storedPacket.amount() - xfered);
+
+                return true;
             }
         }
+
+        return false;
     }
 
     /**
      * Attempts to move a packet to this conduit from the specified direction.
+     * Obeys the routing paradigm set forth in the documentation for {@link #exportToNeighbor(Direction)}.
      */
     protected boolean tryAcceptPacket(ConduitPowerPacket packet, Direction d)
     {
-        if(this.storedPacket == null) {
-            this.storedPacket = new ConduitPowerPacket(d, packet.amount(), false);
+        // TODO validate logic
+        // Register sequential insertions, and disallow acceptance of new packets if the limit has been reached
+        if(lastPacketInsertion == d) {
+            if(sequentialInsertionCount >= MAX_CONSECUTIVE_INSERTIONS) return false;
+            sequentialInsertionCount++;
+        }else { // If this insertion is non-sequential, update the insertion marker and clear the counter
+            lastPacketInsertion = d;
+            sequentialInsertionCount = 0;
+        }
+
+        // This is a non-sequential insertion, so try to insert the packet
+        if (this.storedPacket == null) {
+            this.storedPacket = new ConduitPowerPacket(d, packet.amount());
             return true;
         }
 
@@ -150,33 +185,28 @@ public abstract class BasePowerConduitBE extends PoweredMachineBE
         return false;
     }
 
-    /**
-     * Randomizes the given Direction array.
-     */
-    protected Direction[] shuffle(Direction[] dirs)
-    {
-        // This is an implementation of the general Fisher-Yates shuffle algorithm
-        int index;
-        Direction tmp;
-        for(int i = dirs.length - 1; i > 0; i--)
-        {
-            index = rng.nextInt(i + 1);
-            tmp = dirs[index];
-            dirs[index] = dirs[i];
-            dirs[i] = tmp;
-        }
+    protected static final HashMap<Direction, BooleanProperty> dirMap = generateDirMap();
 
-        return dirs;
+    private static HashMap<Direction, BooleanProperty> generateDirMap()
+    {
+        HashMap<Direction, BooleanProperty> map = new HashMap<>();
+        map.put(Direction.UP, BlockStateProperties.UP);
+        map.put(Direction.DOWN, BlockStateProperties.DOWN);
+        map.put(Direction.NORTH, BlockStateProperties.NORTH);
+        map.put(Direction.SOUTH, BlockStateProperties.SOUTH);
+        map.put(Direction.EAST, BlockStateProperties.EAST);
+        map.put(Direction.WEST, BlockStateProperties.WEST);
+        return map;
     }
 }
 
 /**
  * Represents a packet of "energy" which is used to abstract energy transfer inside the conduit network.
  */
-record ConduitPowerPacket(Direction origin, int amount, boolean imported){
+record ConduitPowerPacket(Direction origin, int amount){
     @Override
     public String toString() {
-        return "ConduitPacket: power " + amount + "; origin " + origin.getName() + "; imported: " + imported;
+        return "ConduitPacket: power " + amount + "; origin " + origin.getName();
     }
 }
 
